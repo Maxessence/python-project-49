a3dd8bbcd4d9e282ffdc09c9e542d81d9f29c8c0
Ирина Максименко <maxis2007@mail.ru>
2023-05-06 11:33:10 +0300

    Change engine

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -6,21 +6,18 @@ def start_game(game):
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print(game.RULES)  # импортирование правил игры
-    # успешное окончание игры три правильный ответа, остановка один неверный
-    correct_answers = 0
-    while correct_answers < 3:
+    # успешное окончание игры - три правильный ответа, остановка - один неверный
+    for _ in range(3):
         # импортирование аргументов игры
         task_question, correct_answer = game.get_task_arguments()
         print(f'Question: {task_question}')
         user_answer = input('Your answer: ')
         if user_answer == correct_answer:
             print('Correct!')
-            correct_answers += 1
         else:
-            break
-    if correct_answers == 3:
-        print(f"Congratulations, {name}!")
-    else:
-        print(f"'{user_answer}' is wrong answer ;(."
+            return print(f"'{user_answer}' is wrong answer ;(."
               f"Correct answer was '{correct_answer}'.\n"
               f"Let's try again, {name}!")
+    print(f"Congratulations, {name}!")
+  
+        
