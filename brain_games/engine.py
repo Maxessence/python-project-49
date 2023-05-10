39ce4f01418a420bbf85e44d01cfb9da2157b861
Ирина Максименко <maxis2007@mail.ru>
2023-05-10 20:14:23 +0300

    Delite commets

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -5,10 +5,9 @@ def start_game(game):
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    print(game.RULES)  # импортирование правил игры
-    # успешное окончание игры - три правильный ответа, остановка - один неверный
-    for _ in range(3):
-        # импортирование аргументов игры
+    print(game.RULES)
+    COUNT = 3
+    for _ in range(COUNT):
         task_question, correct_answer = game.get_task_arguments()
         print(f'Question: {task_question}')
         user_answer = input('Your answer: ')
