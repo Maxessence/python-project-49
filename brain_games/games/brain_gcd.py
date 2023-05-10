a8ebd9288b0b290a038a9bedb7fc6b29932f66cc
Ирина Максименко <maxis2007@mail.ru>
2023-05-10 20:26:12 +0300

    Change logic, new func

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -3,15 +3,18 @@ from random import randint
 RULES = "Find the greatest common divisor of given numbers."
 
 
-def get_task_arguments():
-    number1 = randint(1, 100)
-    number2 = randint(1, 100)
-    task_question = f"{number1} {number2}"
+def is_nod(number1, number2):
     while number1 != 0 and number2 != 0:
         if number1 > number2:
             number1 = number1 % number2
         else:
             number2 = number2 % number1
-    correct_answer = str(number1 + number2)
-    # передача значений в модуль логики игры
+    return number1 + number2
+
+
+def get_task_arguments():
+    number1 = randint(1, 100)
+    number2 = randint(1, 100)
+    task_question = f"{number1} {number2}"
+    correct_answer = str(is_nod(number1, number2))
     return task_question, correct_answer
